311f702863d80ad0e6a93c7b045ae0b7deb0c400
weiweishao <weishao@umich.edu>
2018-09-19 10:37:50 -0400

    magic_eight.py

---
+++ magic_eight.py
@@ -0,0 +1,13 @@
+# SI 507 Hw3
+# Group Member:
+# Person A:
+# Person B:
+# Section 003: Wednesday 10:00 - 11:30 AM
+
+import random
+
+# create a function that ask users "What is your question?" and saves the response
+def get_question():
+    print ("What is your question?")
+    question = str(input("Type your question here: "))
+    return question
